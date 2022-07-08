b265c2d1dc4ebd36c5b236086d7e9da2c5b30f93
Patrick Mahloy <patrick.mahloy@gmail.com>
2022-07-07 20:32:08 -0400

    added Font Awesome Icons

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false;
+
 import '../styles/globals.css';
 import 'tailwindcss/tailwind.css';
+
 
 function MyApp({ Component, pageProps }) {
 	return <Component {...pageProps} />;
