230b24b0ae20f5000cf2b5e019c4b3b315843621
Patrick Mahloy <patrick.mahloy@gmail.com>
2022-07-29 22:00:38 -0400

    added global font, reset.css

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,16 @@ import '@fortawesome/fontawesome-svg-core/styles.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
 config.autoAddCss = false;
 
+import DefaultLayout from '../layouts/DefaultLayout';
+import '../styles/reset.css';
 import '../styles/globals.css';
-import 'tailwindcss/tailwind.css';
 
-
-function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+function MyApp() {
+	return (
+		<>
+      <DefaultLayout />
+		</>
+	);
 }
 
 export default MyApp;
